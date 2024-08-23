981f486c67d9b0da9fc0b42d753a5c50503a7f97
Line Regene <l.regene27@gmail.com>
2024-08-23 15:14:11 +0700

    Portfolio Second Commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import path from "path";
 
 export default {
   root: resolve(__dirname, "src"),
+  publicDir: resolve(__dirname, "public"),
   resolve: {
     alias: {
       "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
