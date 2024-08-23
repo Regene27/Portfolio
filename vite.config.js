0a47e719411b7b67e7415eeb44667ae76facde0d
Line Regene <l.regene27@gmail.com>
2024-08-23 14:46:10 +0700

    Portfolio First Commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { resolve } from "path";
+import path from "path";
+
+export default {
+  root: resolve(__dirname, "src"),
+  resolve: {
+    alias: {
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+    },
+  },
+  server: {
+    port: 8080,
+    hot: true,
+  },
+  build: {
+    outDir: resolve(__dirname, "dist"),
+  },
+};
